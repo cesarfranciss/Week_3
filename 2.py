8bbfe38b102009cfdb260667bd65b0891fda1517
Cesar Francisco Alves Freitas <6freic59@solent.ac.uk>
2022-10-21 12:52:34 +0100

    week 2 first topic

---
+++ 2.py
@@ -0,0 +1,6 @@
+num1= (int(input("enter the first whole number")))
+num2= (int(input("enter the second number")))
+num3= (int(input("enter the third number")))
+
+if (num1,num2,num3%2==0):
+   print("Odd")
---
+++ Week2.py
@@ -0,0 +1,46 @@
+
+first_number= (int(input("Type an integer number:")))
+second_number= (int(input("Type a second integer number:")))
+
+if first_number > second_number:
+    print("First is bigger!")
+elif first_number < second_number:
+    print("First is smaller!")
+else:
+    print("Both are equal!")
+
+print("Done!")
+
+entity=input("Wich one do you identify more? (Human, Robot, or animal?)")
+
+if entity == "Human":
+    print("You are a human!")
+elif entity == "Robot":
+    print("You are a robot!")
+elif entity == "Animal":
+    print("You are an animal!")
+else:
+    print("I do not know what you are!")
+
+print("Analysis complete.")
+
+
+direction= input("Towards which direction should I paint (up, down, left or right)?")
+
+if direction == "Up":
+    print("Right direction")
+else:
+    print("Wrong direction")
+
+whole_number=(int(input("Please enter a whole number.")))
+if (whole_number%2==0):
+   print("Odd number")
+else:
+    print("Even number")
+
+num1= (int(input("enter the first whole number")))
+num2= (int(input("enter the second number")))
+num3= (int(input("enter the third number")))
+
+if (num1,num2,num3%2==0)
+   print("Odd")
